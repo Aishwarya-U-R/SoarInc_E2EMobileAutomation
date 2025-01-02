c48676416b9b8237eb24b8a387f4f86f9f8db551
Aishwarya UR <aishwarya@appsmith.com>
2025-01-03 03:03:24 +0530

    update

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -22,7 +22,7 @@ export const config: WebdriverIO.Config = {
   // The path of the spec files will be resolved relative from the directory of
   // of the config file unless it's absolute.
   //
-  specs: ["./test/specs/WikiAppTest.e2e.ts"],
+  specs: ["./tests/specs/WikiAppTest.e2e.ts"],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
